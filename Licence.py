655e562248aac62fd4197b8e5fd6ada307b3e207
SKapal <sahilkapal@cmail.carleton.ca>
2020-03-23 15:44:48 -0400

    get license plate of image, enlarge and sharpen

--- Licence.py
+++ Licence.py
@@ -3,7 +3,7 @@ import numpy as np
 # Read the image file
 from skimage.exposure import rescale_intensity
 
-image = cv2.imread('Images/blurry_plate3.jpg', 0)
+image = cv2.imread('Images/blurry_plate.jpg', 0)
 
 # works for 1,2,5
 image = cv2.resize(image, (325, 325))
@@ -17,7 +17,7 @@ edged = cv2.Canny(gray, 170, 200)
 
 
 # Find contours based on Edges
-cnts, hierarchy = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+cnts, hierarchy  = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:10]  # sort contours based on their area keeping minimum
 # required area as '30' (anything smaller than this will not be considered)
 NumberPlateCnt = None  # we currently have no Number plate contour
@@ -33,12 +33,48 @@ for c in cnts:
 # rc = cv2.drawContours(image, [NumberPlateCnt], -1, 255, 1)
 rc = cv2.minAreaRect(NumberPlateCnt)
 box = cv2.boxPoints(rc)
+
 for p in box:
     pt = (p[0], p[1])
-    print(pt)
-    cv2.circle(image, pt, 5, (200, 0, 0), 2)
 
 cv2.imshow("Licence Plate Detected", image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+## Sahil's part: extract license plate from image:
+
+# Obtain coordinates of plate:
+topLeft_x = int(box[1][0])
+topLeft_y = int(box[1][1])
+
+bottomLeft_x = int(box[0][0])
+bottomLeft_y = int(box[0][1])
+
+topRight_x = int(box[2][0])
+
+# get width and height
+plate_height = bottomLeft_y - topLeft_y
+plate_width = topRight_x - topLeft_x
+
+# get crop image
+crop_img = image[topLeft_y:topLeft_y + plate_height, bottomLeft_x:bottomLeft_x + plate_width]
+
+cv2.imshow("Cropped License Plate", crop_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+ 
+# increase plate size:
+large_plate = cv2.resize(crop_img, (0,0), fx=3, fy=3) 
+
+# sharpen cropped image
+kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+
+# Apply kernel to image 
+sharpened_image = cv2.filter2D(large_plate, -1, kernel)
+
+cv2.imshow("sharpened plate", sharpened_image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
